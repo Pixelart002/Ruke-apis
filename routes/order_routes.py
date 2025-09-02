0013166d9e9b85c58a90e7faccf1a3b59439bac0
kyro <j>
2025-09-02 19:25:10 +0530

    update order_routes.py

--- routes/order_routes.py
+++ routes/order_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+    db.session.commit()
+    return jsonify({'message':'Order created','order_id':order.order_id,'total':total})from flask import Blueprint, jsonify
 from database import db
 from models.cart import CartItem
 from models.order import Order
@@ -11,12 +12,16 @@ order_bp = Blueprint('order_bp', __name__)
 @token_required
 def checkout(current_user):
     cart_items = CartItem.query.filter_by(user_id=current_user.user_id).all()
-    if not cart_items: return jsonify({'message':'Cart empty'}),400
-    total = sum([item.quantity*item.product.price for item in cart_items])
-    order = Order(user_id=current_user.user_id,total=total)
+    if not cart_items:
+        return jsonify({'message':'Cart empty'}), 400
+
+    total = sum([item.quantity * item.product.price for item in cart_items])
+    order = Order(user_id=current_user.user_id, total=total)
     db.session.add(order)
+
     for item in cart_items:
-      item.product.stock -= item.quantity  # reduce stock
+        item.product.stock -= item.quantity  # reduce stock
         db.session.delete(item)  # remove from cart
+
     db.session.commit()
-    return jsonify({'message':'Order created','order_id':order.order_id,'total':total})
+    return jsonify({'message':'Order created', 'order_id': order.order_id, 'total': total})
