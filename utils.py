fea8f041d4fc6719aeb3b7ff22f40998e61312ac
kyro <v>
2025-09-02 15:23:55 +0530

    update utils.py

--- utils.py
+++ utils.py
@@ -17,10 +17,10 @@ def generate_token() -> str:
 def token_expiry_datetime(days: int = 7):
     return datetime.utcnow() + timedelta(days=days)
 
-def is_browser_safe(request: Request) -> bool:
-    ua = request.headers.get("user-agent", "") or ""
-    ua_l = ua.lower()
-    block_keywords = ["curl", "wget", "python-requests", "scanner", "bot"]
-    if any(k in ua_l for k in block_keywords):
-        return False
-    return True
+#def is_browser_safe(request: Request) -> bool:
+    #ua = request.headers.get("user-agent", "") or ""
+  #  ua_l = ua.lower()
+   # block_keywords = ["curl", "wget", "python-requests", "scanner", "bot"]
+  #  if any(k in ua_l for k in block_keywords):
+    #    return False
+  #  return True
