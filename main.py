b4bc64716ad27d458c5880cf644bcfff9f809143
kyro <t>
2025-09-05 09:38:12 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-from .schemas import User, Product, Order
+from schemas import User, Product, Order
 from .crud import create_user, get_user_by_email, add_product, list_products, create_order
 from .utils import generate_invoice_pdf, validate_coupon
 
