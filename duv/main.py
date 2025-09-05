f89476bd77c2a00cbcad313b327e25cf62885486
kyro <u>
2025-09-05 12:32:50 +0530

    update

--- duv/main.py
+++ duv/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-from schemas import User, Product, Order
-from crud import create_user, get_user_by_email, add_product, list_products, create_order
-from utils import generate_invoice_pdf, validate_coupon
+from app.schemas import User, Product, Order
+from app.crud import create_user, get_user_by_email, add_product, list_products, create_order
+from app.utils import generate_invoice_pdf, validate_coupon
 
 app = FastAPI(title="GodMode E-Commerce API")
 
