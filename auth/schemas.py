d5b36bab058801ec99abceca6efd530bf2e614a4
kyro <9013ms@gmail.com>
2025-09-21 01:05:07 +0530

    update schemas.py

--- auth/schemas.py
+++ auth/schemas.py
@@ -4,6 +4,8 @@ class UserCreate(BaseModel):
     fullname: str = Field(..., min_length=3, max_length=50)
     username: str = Field(..., min_length=3, max_length=20, pattern="^[a-zA-Z0-9_]+$")
 
+    
+
     email: EmailStr
     password: str = Field(..., min_length=6)
 
