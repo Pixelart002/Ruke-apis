ce353aa89bacd9ac6e68ff6ff0922dc50ec0aafd
kyro <9013ms@gmail.com>
2025-09-21 08:06:44 +0530

    update schemas.py

--- auth/schemas.py
+++ auth/schemas.py
@@ -56,26 +56,6 @@ class ResetPasswordRequest(BaseModel):
 
 
 
-###
 
 
 
-# auth/schemas.py
-
-# ... (imports)
-
-class UserCreate(BaseModel):
-    fullname: str = Field(..., min_length=3, max_length=50)
-    # --- YEH LINE ADD KAREIN ---
-    username: str = Field(..., min_length=3, max_length=20, regex="^[a-zA-Z0-9_]+$")
-    email: EmailStr
-    password: str = Field(..., min_length=6)
-
-# ... (UserLogin model waisa hi rahega) ...
-
-class UserInfo(BaseModel):
-    # --- YEH DO LINES ADD/UPDATE KAREIN ---
-    userId: str # MongoDB _id ko string format mein bhejenge
-    username: str
-    fullname: str
-    email: EmailStr
