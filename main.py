6187bf9444965d6bc98643c05f00e06f261a39de
kyro <9013ms@gmail.com>
2025-10-14 10:42:22 +0530

    update

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from routers.users import router as users_router
 from routers.ai import router as ai_router
 from routers.notifications import router as notifications_router
 from routers.feedback import router as feedback_router
-from routers.invoice import router as invoice_router
 
 
 
@@ -39,7 +38,7 @@ app.include_router(users_router)
 app.include_router(ai_router)
 app.include_router(notifications_router)
 app.include_router(feedback_router)
-app.include_router(invoice_router)
+
 
 # --- Root Endpoint ---
 @app.get("/")
